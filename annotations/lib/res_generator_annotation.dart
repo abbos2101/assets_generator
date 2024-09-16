3ed4a3cb57dcabea1c098e7a9c413084dda0b436
abbos2101 <abbos2101@gmail.com>
2024-09-16 11:02:50 +0500

    remove example and annotations

--- annotations/lib/res_generator_annotation.dart
+++
@@ -1,4 +0,0 @@
-library res_generator_annotation;
-
-export 'src/res_images_annotation.dart';
-export 'src/res_words_annotation.dart';
--- example/android/app/src/main/kotlin/uz/abbos/example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package uz.abbos.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
